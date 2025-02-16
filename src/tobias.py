a65ba61efed8c5ac3b4f9e891f0126571ab84bce
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-02-16 20:37:16 +0100

    add version selector

--- src/tobias.py
+++ src/tobias.py
@@ -6,28 +6,40 @@ def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://vimm.net/vault/81222")
+        page.goto("https://vimm.net/vault/17978")
 
         soup = BeautifulSoup(page.content(), "html.parser")
-        select_tag = soup.find("select", {"id": "dl_format"})
 
-        if select_tag:
-            options = [option.text for option in select_tag.find_all("option")]
+        select_version = soup.find("select", {"id": "dl_version"})
+        select_format = soup.find("select", {"id": "dl_format"})
+
+        if select_version:
+            versions = [option.text for option in select_version.find_all("option")]
+            print("Available versions:")
+            for idx, version in enumerate(versions):
+                print(f"{idx}: {version}")
+            version_choice = int(input("Choose version by number: "))
+            selected_version = select_version.find_all("option")[version_choice]["value"]
+            page.select_option("#dl_version", selected_version)
+            print(f"Selected version: {versions[version_choice]}")
+        else:
+            print("Version selector not found.")
+
+        if select_format:
+            formats = [option.text for option in select_format.find_all("option")]
             print("Available formats:")
-            for idx, option in enumerate(options):
-                print(f"{idx}: {option}")
-
-            choice = int(input("Choose format by number: "))
-            selected_value = select_tag.find_all("option")[choice]["value"]
-
-            page.select_option("#dl_format", selected_value)
-            print(f"Selected format: {options[choice]}")
-
-            page.evaluate(f"setFormat('dl_form', '{selected_value}', allMedia);")
-            page.evaluate("confirmPopup(document.forms['dl_form'], 'tooltip4');")
-
+            for idx, fmt in enumerate(formats):
+                print(f"{idx}: {fmt}")
+            format_choice = int(input("Choose format by number: "))
+            selected_format = select_format.find_all("option")[format_choice]["value"]
+            page.select_option("#dl_format", selected_format)
+            print(f"Selected format: {formats[format_choice]}")
         else:
             print("Format selector not found.")
+
+        page.evaluate(f"setMediaId('dl_form', allMedia)")
+        page.evaluate(f"setFormat('dl_form', '{selected_format}', allMedia);")
+        page.evaluate("confirmPopup(document.forms['dl_form'], 'tooltip4');")
 
         input("Press Enter to close...")
         browser.close()
