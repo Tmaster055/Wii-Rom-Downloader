3ea5591d545222c0b5f4a2040da4fe99236d34d8
Tmaster055 <gratzer.tobias3@gmail.com>
2025-04-28 14:59:35 +0200

    Add LICENSE and pyproject.toml

---
+++ wii-roms-tool/src/main.py
@@ -0,0 +1,37 @@
+from common import download_vimms_rom, download_romsfun_rom
+from search_vimms import search_for_rom_vimms
+from search_romsfun import search_for_rom_romsfun
+
+
+def main():
+    print("Welcome to Wii-Rom-Downloader!")
+    print("1: Vimms Lair")
+    print("2: Romsfun")
+
+    while True:
+        answer = input("Which do you choose? ")
+
+        if answer == "1":
+            while True:
+                url = search_for_rom_vimms()
+                if url is None:
+                    pass
+                else:
+                    download_vimms_rom(url)
+                    break
+            break
+        elif answer == "2":
+            while True:
+                url = search_for_rom_romsfun()
+                if url is None:
+                    pass
+                else:
+                    download_romsfun_rom(url)
+                    break
+            break
+        else:
+            print("Invalid input. Please enter 1 or 2.")
+
+
+if __name__ == "__main__":
+    pass
